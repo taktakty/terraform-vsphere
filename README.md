4af02378202e0b8925bb7d05fefcce715ac9acf7
tak <taktakty@gmail.com>
2020-02-29 19:50:18 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+How to use
+==========
+
+## Set environment vairables
+
+```
+export TF_VAR_vsphere_user={user}
+export TF_VAR_vsphere_password={password}
+export TF_VAR_vsphere_server={server}
+```
---
+++ vsphere.tf
@@ -0,0 +1,72 @@
+provider "vsphere" {
+  user           = var.vsphere_user
+  password       = var.vsphere_password
+  vsphere_server = var.vsphere_vcenter
+
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = var.vsphere_datacenter
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.vsphere_cluster
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.vm_datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.vm_network
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.vm_template
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = var.vm_name
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = var.vm_cpus
+  memory   = var.vm_ram
+  guest_id = data.vsphere_virtual_machine.template.guest_id
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  disk {
+    label = "${var.vm_name}.vmdk"
+    size  = data.vsphere_virtual_machine.template.disks.0.size
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+    linked_clone  = var.vm_linked_clone
+
+    customize {
+      timeout = "20"
+
+      linux_options {
+        host_name = var.vm_name
+        domain    = var.vm_domain
+      }
+
+      network_interface {
+        ipv4_address = var.vm_ip
+        ipv4_netmask = var.vm_netmask
+      }
+
+      ipv4_gateway    = var.vm_gateway
+      dns_server_list = [var.vm_dns]
+    }
+  }
+}
